cae54baa34d3db83b40882587595ec3ca3d74992
kienvu15 <88vutrunkien@gmail.com>
2026-01-20 21:09:00 +0700

    attack dunk + Chef Enemy

---
+++ Assets/0_Game/1_Srcipts/Card/Condition/EffectScripts/DrunkEffectData.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "DrunkEffect", menuName = "Cards/Effects/Apply_Drunk")]
+public class DrunkEffectData : EffectData
+{
+    [Tooltip("Số lượt tồn tại Drunk")]
+    public int duration = 2;
+
+    public CardType vfxType = CardType.Magic;
+
+    public override bool Apply(Character self, Character target, ManaSystem manaSystem, Deck deck)
+    {
+        Character realTarget = target != null ? target : self;
+        if (realTarget == null) return false;
+
+        Condition drunk = new Condition
+        {
+            type = ConditionType.Drunk,
+            duration = duration
+        };
+
+        bool isFromPlayer = self is Player;
+        realTarget.AddCondition(drunk, isFromPlayer, vfxType);
+
+        //Debug.Log($"[DrunkEffect] {self.name} applied Drunk({duration}) to {realTarget.name}");
+        return true;
+    }
+
+    public override int GetIntentValue()
+    {
+        return duration;
+    }
+}
---
+++ Assets/0_Game/1_Srcipts/Card/Effects/AttackEffect_Drunk.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "AttackEffect_Drunk", menuName = "Cards/Effects/Attack_Drunk")]
+public class AttackEffect_Drunk : EffectData, IOverrideValue
+{
+    public int damage = 6;
+    public CardType vfxType = CardType.Mellee;
+
+    [Tooltip("Số lượt tồn tại Drunk")]
+    public int drunkDuration = 1;
+
+    public override bool Apply(Character self, Character target, ManaSystem manaSystem, Deck deck)
+    {
+        if (target == null)
+            return false;
+
+        bool hit = self.DealDamage(target, damage, vfxType);
+
+        if (hit)
+        {
+            Condition drunk = new Condition
+            {
+                type = ConditionType.Drunk,
+                duration = drunkDuration,
+                level = 1
+            };
+
+            bool isFromPlayer = self is Player;
+            target.AddCondition(drunk, isFromPlayer, CardType.Special);
+
+            //Debug.Log($"[AttackEffect_Drunk] Applied Drunk({drunkDuration}) to {target.name}");
+        }
+
+        return hit;
+    }
+
+    // ========== Intent ==========
+    public override int GetIntentValue()
+    {
+        return damage;
+    }
+
+    // ========== IOverrideValue ==========
+    public int GetIntentValue(int? overrideValue = null)
+    {
+        return overrideValue ?? damage;
+    }
+
+    public void ApplyWithOverride(Character self, Character target, int overrideValue)
+    {
+        if (target == null) return;
+
+        int old = damage;
+        damage = overrideValue;
+
+        bool hit = self.DealDamage(target, damage, vfxType);
+
+        if (hit)
+        {
+            Condition drunk = new Condition
+            {
+                type = ConditionType.Drunk,
+                duration = drunkDuration,
+                level = 1
+            };
+
+            bool isFromPlayer = self is Player;
+            target.AddCondition(drunk, isFromPlayer, CardType.Special);
+        }
+
+        damage = old;
+    }
+}
